1934e92d0ec28d03d8000a92e180689fc8e47696
Lamichhane, Abishek <saksheekirans@gmail.com>
2025-02-17 18:45:54 -0600

    Getting started with the landing page for the website

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { LoginSignUp } from './Components/LoginSignup/LoginSignUp';  // Adjusted to match the file name
+
+function App() {
+  return (
+    <div>
+      <LoginSignUp /> 
+    </div>
+  );
+}
+
+export default App;
